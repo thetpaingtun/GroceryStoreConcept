cc03b0c7132d5c70becf3ca575bd38e382fbcccc
Thet Paing Tun <thetpaingtun93@gmail.com>
2020-02-29 14:24:27 +0800

    changes

--- app/src/main/java/com/gmail/tpt/grocerystore/MainActivity.kt
+++ app/src/main/java/com/gmail/tpt/grocerystore/MainActivity.kt
@@ -5,7 +5,10 @@ import android.view.Menu
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.constraintlayout.widget.Placeholder
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -13,6 +16,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_test.*
 import kotlinx.android.synthetic.main.activity_test.view.*
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,6 +37,13 @@ class MainActivity : AppCompatActivity() {
 
 
         setupViews()
+
+        motionRoot.setTransitionListener(object : TransitionAdapter() {
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                Logger.d("progress => " + p3)
+                motionLayoutDetail.progress = abs(p3)
+            }
+        })
 
     }
 
